b415ee3537f971071e810067148408b9e7b264ae
OliwierSellig54 <oliwierandrzej.sellig@gmail.com>
2025-04-07 13:51:37 +0200

    feat: create Blog Page Hero

--- apps/sanity/schema/singleTypes/Blog_Page.ts
+++ apps/sanity/schema/singleTypes/Blog_Page.ts
@@ -1,15 +1,97 @@
 import { Book } from 'lucide-react'
-import { createPageSchema } from '../templates/pageTemplate'
+import { defineType } from 'sanity'
+import { ComposeIcon } from '@sanity/icons'
+import { defineField } from 'sanity'
+import { defineSlugForDocument } from '../../utils/define-slug-for-document'
+import { SearchIcon } from '@sanity/icons'
+import { getLanguagePreview } from '../../structure/languages'
 
 const name = 'Blog_Page'
 const title = 'Strona bloga'
 
-export default createPageSchema({
+export default defineType({
   name,
+  type: 'document',
   title,
   icon: Book,
-  slugs: {
-    pl: '/pl/blog',
-    en: '/en/blog',
+  options: { documentPreview: true },
+  fields: [
+    defineField({
+      name: 'language',
+      type: 'string',
+      readOnly: true,
+      hidden: true,
+    }),
+    ...defineSlugForDocument({
+      slugs: {
+        pl: '/pl/blog',
+        en: '/en/blog',
+      },
+      group: 'content',
+    }),
+    defineField({
+      name: 'heading',
+      type: 'Heading',
+      title: 'Nagłówek',
+      description: 'Główny nagłówek na tej stronie nie jest widoczny, umieszczamy go głównie w kontekście SEO',
+      group: 'content',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'subheading',
+      type: 'Heading',
+      title: 'Podnagłówek',
+      description: 'Podnagłówek na tej stronie jest widoczny ponad listą wpisów na blogu, ale pod wyróżnionym wpisem.',
+      group: 'content',
+      fieldset: 'listing',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'paragraph',
+      type: 'Heading',
+      title: 'Paragraf',
+      group: 'content',
+      fieldset: 'listing',
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: 'components',
+      type: 'components',
+      title: 'Komponenty podstrony',
+      group: 'content',
+    }),
+    defineField({
+      name: 'seo',
+      type: 'seo',
+      title: 'SEO',
+      group: 'seo',
+    }),
+  ],
+  groups: [
+    {
+      name: 'content',
+      title: 'Treść',
+      icon: ComposeIcon,
+    },
+    {
+      name: 'seo',
+      title: 'SEO',
+      icon: SearchIcon,
+    },
+  ],
+  fieldsets: [
+    {
+      name: 'listing',
+      title: 'Lista wpisów',
+    },
+  ],
+  preview: {
+    select: {
+      title: 'name',
+      language: 'language',
+    },
+    prepare: ({ title, language }) => {
+      return getLanguagePreview({ title, languageId: language })
+    },
   },
 })
