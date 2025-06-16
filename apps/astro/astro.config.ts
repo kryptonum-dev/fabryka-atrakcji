efd3e9446d4c074c9c53de41bef41692b2d3713d
OliwierSellig54 <oliwierandrzej.sellig@gmail.com>
2025-06-16 07:53:04 +0200

    feat: update astro config to fix ISR

--- apps/astro/astro.config.ts
+++ apps/astro/astro.config.ts
@@ -41,7 +41,7 @@ export default defineConfig({
             exclude: [
               /^\/api\/.+/,
               /^\/pl\/koszyk/,
-              /^\/pl\/hotele$/,
+              /^\/pl\/hotele\/?(\?.*)?$/,
               /^\/pl\/hotele\/strona/,
               /^\/pl\/integracje$/,
               /^\/pl\/integracje\/strona/,
